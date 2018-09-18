7c8e3a03e6603d8437c93b1f31c615a147f0e299
nbright@rogers.com <nbright@rogers.com>
2018-09-17 23:46:03 -0400

    Quick commit to make the logger create the folder, if it is missing

--- src/mapmaker/LogData.java
+++ src/mapmaker/LogData.java
@@ -5,9 +5,9 @@
  */
 package mapmaker;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.logging.FileHandler;
-import java.util.logging.Filter;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
@@ -22,8 +22,12 @@ interface LogData {
     abstract void initiateLogging();
     
     static void initiateLogging(String className, Logger logger){
-        String CWD = System.getProperty("user.dir");
         try {
+            String CWD = System.getProperty("user.dir");
+            if(!(new File(CWD + "/logs/").exists())){
+                System.out.println("Directory not found: " + new File(CWD + "/logs/").getAbsolutePath());
+                new File(CWD + "/logs/").mkdir();
+            }
             FileHandler fileHandler = new FileHandler(CWD + "/logs/" + className + ".log");
             logger.setFilter((LogRecord lr) -> {
                 if(MapMaker.allLogging){
