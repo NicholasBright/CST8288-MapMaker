42c36001c1499cc68d13e057e722fe20be0c6000
nbright@rogers.com <nbright@rogers.com>
2018-11-09 14:48:11 -0500

    Loading is now functional as well

--- src/mapmaker/ResourceManager.java
+++ src/mapmaker/ResourceManager.java
@@ -6,12 +6,16 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import javafx.scene.paint.Paint;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
@@ -20,6 +24,8 @@ import mapmaker.mapelement.Path;
 import mapmaker.mapelement.Room;
 
 public class ResourceManager{
+    private static final Logger LOGGER = Logger.getLogger(ResourceManager.class.getName());
+    
     private static final ResourceManager RESOURCE_MANAGER = new ResourceManager();
     
     private final String RESOURCE_PATH = "resources/";
@@ -28,6 +34,17 @@ public class ResourceManager{
     private final String MAPS_PATH = "maps/";
     
     private ResourceManager(){
+        try {
+            File logFolder = new File("logs/");
+            if(!logFolder.exists())
+                logFolder.mkdir();
+            FileHandler fh = new FileHandler("logs/" + ResourceManager.class.getSimpleName() + ".txt");
+            LOGGER.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
+        } catch (IOException | SecurityException ex) {
+            LOGGER.log(Level.SEVERE, null, ex);
+        }
     }
     
     public String loadTxtToString(String fileName) throws FileNotFoundException{
@@ -56,9 +73,13 @@ public class ResourceManager{
             return;
         
         StringBuilder sb = new StringBuilder();
-        ma.getRooms().forEach((room) -> {
+        int ID = 0;
+        for(Room room : ma.getRooms()){
             sb.append("sides ");
             sb.append(room.getNumSides());
+            sb.append(System.lineSeparator());
+            sb.append("ID ");
+            sb.append(ID++);
             sb.append(System.lineSeparator());
             sb.append("strokeWidth ");
             sb.append(room.getStrokeWidth());
@@ -96,7 +117,7 @@ public class ResourceManager{
                 sb.append(" ");
             }
             sb.append(System.lineSeparator());
-        });
+        }
         
         try{
             if(!file.exists())
@@ -113,23 +134,85 @@ public class ResourceManager{
         if (file==null || !file.exists())
             return;
         try{
-            
             //no parallel (threading) here but this is safer
             AtomicInteger index = new AtomicInteger(0);  
             //index.getAndIncrement()/5 means every 5 elements increases by 1
             //allowing for every 5 element placed in the same key
             //for each line in file group every 5 and pass to map area
             Map<Integer, Room> roomMap = new HashMap<>();
-            Files.lines(file.toPath()).collect( Collectors.groupingBy( l->index.getAndIncrement()/6)).forEach((i,l)->{
+            Map<Room, String[]> lockMap = new HashMap<>();
+            Files.lines(file.toPath()).collect( Collectors.groupingBy( l->index.getAndIncrement()/7)).forEach((i,l)->{
                 Room r = new Room();
                 for(String s : l){
-                   String[] values = s.split(" ");
-                   switch(values[0]){
-                       case "sides":
-                           r.setNumSides(Integer.parseInt(values[1]));
-                           break;
+                    String[] values = s.split(" ");
+                    switch(values[0]){
+                        case "sides":
+                            r.setNumSides(Integer.parseInt(values[1]));
+                            break;
+                        case "ID":
+                            roomMap.put(Integer.parseInt(values[1]), r);
+                            break;
+                        case "strokeWidth":
+                            r.setStrokeWidth(Double.parseDouble(values[1]));
+                            break;
+                        case "fill":
+                            r.setFill(Paint.valueOf(values[1]));
+                            break;
+                        case "stroke":
+                            r.setFill(Paint.valueOf(values[1]));
+                            break;
+                        case "locks":
+                            lockMap.put(r, values);
+                            break;
+                        case "points":
+                            double cX = 0.0;
+                            double cY = 0.0;
+                            for(int p=1; p+1< values.length; p+=2){
+                                cX += Integer.parseInt(values[p]);
+                                cY += Integer.parseInt(values[p+1]);
+                            }
+                            cX = cX/((values.length-1)/2);
+                            cY = cY/((values.length-1)/2);
+                            r.normalizeShape(cX, cY, Integer.parseInt(values[1]), Integer.parseInt(values[2]));
+                            break;
+                        default:
+                            LOGGER.log(Level.WARNING, "Unknown line value \"{0}\" in file \"{1}\"", new String[]{values[0], file.toString()});
+                            break;
                    }
                }
+                ma.add(r);
+            });
+            lockMap.keySet().forEach((room)->{
+                List<Path> paths = room.getPaths();
+                String[] locks = lockMap.get(room);
+                for(int p=1; p < locks.length; p++){
+                    boolean exists = false;
+                    for(Path path : paths){
+                        if(!exists){
+                            if(path.getStart() == room){
+                                if(path.getEnd() == roomMap.get(Integer.parseInt(locks[p]))){
+                                    exists = true;
+                                    break;
+                                }
+                            }
+                            else if(path.getEnd() == room){
+                                if(path.getStart() == roomMap.get(Integer.parseInt(locks[p]))){
+                                    exists = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    if(!exists){
+                        Room connecting = roomMap.get(Integer.parseInt(locks[p]));
+                        Path newPath = new Path(room, connecting);
+                        room.addPath(newPath);
+                        connecting.addPath(newPath);
+                        ma.getChildren().add(newPath);
+                        ma.getChildren().add(newPath.getStartPoint());
+                        ma.getChildren().add(newPath.getEndPoint());
+                    }
+                }
             });
         }catch( IOException e){
             e.printStackTrace();
